85c1152f4b1dbc8f5d63625238fa1a5528f30ecf
a24efrenpb-glitch <a24efrenpb@iessanclemente.net>
2025-11-28 14:31:49 +0100

    dhcp terminado

--- ned/entrypoint.sh
+++ ned/entrypoint.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-ip route del default via 192.168.10.1
-ip route add default via 192.168.10.254
+# Ajusta la ruta por defecto hacia el router del enlace
+ip route del default via 192.168.10.1 || true
+ip route add default via 192.168.10.254 || true
 
-kea-dhcp4 -c /etc/kea/kea-dhcp4.conf -d/
+# Elimina archivos PID antiguos si existen
+rm -f /var/run/kea/kea-dhcp4.kea-dhcp4.pid
+rm -f /var/run/kea/kea-dhcp-ddns.kea-dhcp-ddns.pid
+
+# Ejecuta kea-dhcp4 en modo debug usando el archivo de configuración montado
+kea-dhcp-ddns -c /etc/kea/kea-dhcp-ddns.conf -d &
+
+kea-dhcp4 -c /etc/kea/kea-dhcp4.conf -d
